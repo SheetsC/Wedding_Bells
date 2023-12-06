5d96c2ea906200d86220db888da25ba468bd547a
Connor Sheets <connorsheets32@gmail.com>
2023-12-06 09:58:55 -0700

    time to test token creation and auth

--- Backend/WeddingBells.API/Controllers/LoginController.cs
+++ Backend/WeddingBells.API/Controllers/LoginController.cs
@@ -1,37 +1,107 @@
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using WeddingBells.API.Data_;
+// using WeddingBells.API.Models;
+// using BCrypt.Net;
+
+// namespace WeddingBells.API.controllers;
+// [ApiController]
+// [Route("[controller]")]
+// public class LoginController : ControllerBase
+// {
+//     public readonly WeddingBellsContext _context;
+
+//     public LoginController(WeddingBellsContext context)
+//     {
+//         _context = context;
+//     }
+//     [HttpPost]
+//     public async Task<IActionResult> Login([FromBody]LoginModel loginModel )
+//     {
+//         var responseMatch = await _context.Hosts.FirstOrDefaultAsync(u => u.EmailAddress==loginModel.EmailAddress);
+//         if (responseMatch == null && loginModel.EmailAddress != responseMatch?.EmailAddress)
+//         {
+//             return Unauthorized(new{message = "No assosicated Email"});
+//         }
+//         else if (responseMatch != null && BCrypt.Net.BCrypt.Verify(loginModel.Password, responseMatch.Password))
+//         {
+//             responseMatch.LoggedIn = true;
+//             await _context.SaveChangesAsync();
+//             return Ok(new {message = "Login Successful"});
+//         }
+//         else 
+//         {
+//             return Unauthorized(new{message = "Invalid Password"});
+//         }
+//     }   
+// }
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WeddingBells.API.Data_;
 using WeddingBells.API.Models;
 using BCrypt.Net;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
 
-namespace WeddingBells.API.controllers;
+namespace WeddingBells.API.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class LoginController : ControllerBase
 {
-    public readonly WeddingBellsContext _context;
+    private readonly WeddingBellsContext _context;
 
     public LoginController(WeddingBellsContext context)
     {
         _context = context;
     }
+
     [HttpPost]
-    public async Task<IActionResult> Login([FromBody]LoginModel loginModel )
+    public async Task<IActionResult> Login([FromBody]LoginModel loginModel)
     {
-        var responseMatch = await _context.Hosts.FirstOrDefaultAsync(u => u.EmailAddress==loginModel.EmailAddress);
-        if (responseMatch == null && loginModel.EmailAddress != responseMatch?.EmailAddress)
+        var responseMatch = await _context.Hosts.FirstOrDefaultAsync(u => u.EmailAddress == loginModel.EmailAddress);
+        if (responseMatch == null)
         {
-            return Unauthorized(new{message = "No assosicated Email"});
+            return Unauthorized(new { message = "No associated Email" });
         }
-        else if (responseMatch != null && BCrypt.Net.BCrypt.Verify(loginModel.Password, responseMatch.Password))
+        else if (BCrypt.Net.BCrypt.Verify(loginModel.Password, responseMatch.Password))
         {
-            responseMatch.LoggedIn = true;
-            await _context.SaveChangesAsync();
-            return Ok(new {message = "Login Successful"});
+            var token = GenerateJwtToken(responseMatch.HosterId.ToString());
+            return Ok(new { message = "Login Successful", token });
         }
-        else 
+        else
         {
-            return Unauthorized(new{message = "Invalid Password"});
+            return Unauthorized(new { message = "Invalid Password" });
         }
-    }   
+    }
+
+    private static string GenerateJwtToken(string hosterId)
+    {
+        var tokenPass = Environment.GetEnvironmentVariable("tokenPass") 
+            ?? throw new InvalidOperationException("JWT token password not found in environment variables");
+        var issuer = Environment.GetEnvironmentVariable("tokenIssuer")
+            ?? throw new InvalidOperationException("JWT issuer not found in environment variables");
+        var audience = Environment.GetEnvironmentVariable("tokenAud")
+            ?? throw new InvalidOperationException("JWT audience not found in environment variables");
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenPass));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, hosterId),
+            // Add other claims as needed
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
 }
