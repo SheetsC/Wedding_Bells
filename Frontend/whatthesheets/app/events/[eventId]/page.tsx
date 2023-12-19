61e34cbd3c2983253c174592558ab9674fdebcf3
Connor Sheets <connorsheets32@gmail.com>
2023-12-19 09:59:55 -0700

    attempt to access Frontend event data

--- Frontend/whatthesheets/app/events/[eventId]/page.tsx
+++ Frontend/whatthesheets/app/events/[eventId]/page.tsx
@@ -6,13 +6,18 @@ import { useEffect, useState } from 'react';
 // Define the interface for params
 interface EventPageParams {
   eventId: string;
+  title: string;
+  location: string;
+  date: string;
+  description: string;
 }
 
 // Type the props of the function
 const Events: NextPage<{ params: EventPageParams }> = ({ params }) => {
   const eventId = parseInt(params.eventId);
 
-  const [eventData, setEventData] = useState(null);
+
+  const [eventData, setEventData] = useState(params);
 
   useEffect(() => {
     if (eventId) {
@@ -30,7 +35,7 @@ const Events: NextPage<{ params: EventPageParams }> = ({ params }) => {
 
   return (
     <div>
-      <h1>Event ID: {eventId}</h1>
+      <h1>Event ID: {eventData.title}</h1>
       {/* Additional content */}
       {/* <h1>Event Name: {eventData}</h1> */}
     </div>
