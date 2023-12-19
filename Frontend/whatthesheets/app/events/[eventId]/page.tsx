0299af0fbf2aabdb20b4ba45b0343c0c0f69c62c
Connor Sheets <connorsheets32@gmail.com>
2023-12-19 13:32:57 -0700

    attempt to access Frontend event data with new initializer

--- Frontend/whatthesheets/app/events/[eventId]/page.tsx
+++ Frontend/whatthesheets/app/events/[eventId]/page.tsx
@@ -1,47 +1,87 @@
 "use client";
-//change for git
+
+// import { NextPage } from 'next';
+// import { useEffect, useState } from 'react';
+
+// // Define the interface for params
+// interface EventPageParams {
+//   eventId: number;
+//   title: string;
+//   location: string;
+//   date: string;
+//   description: string;
+
+// }
+
+// // Type the props of the function
+// const Events: NextPage<{ params: EventPageParams }> = ({ params }) => {
+//   const eventId = params.eventId;
+
+
+//   const [eventData, setEventData] = useState(params);
+
+//   useEffect(() => {
+//     if (eventId) {
+//       // Fetch event data
+//       fetch(`https://weddingbells-api.onrender.com/event/${eventId}`)
+//       //fetch(`http://localhost:8080/events/${eventId}`)
+//         .then(response => response.json())
+//         .then(data => {
+//           setEventData(data);
+//           console.log(data);
+//         })
+//         .catch(error => console.error('Error fetching event:', error));
+//     }
+//   }, [eventId]);
+
+//   return (
+//     <div>
+//       <h1>Event ID: {eventData.title}</h1>
+//       {/* Additional content */}
+//       {/* <h1>Event Name: {eventData}</h1> */}
+//     </div>
+//   );
+// };
+
+// export default Events;
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 
-// Define the interface for params
-interface EventPageParams {
-  eventId: string;
+// Assuming this interface matches the structure of data returned from your API
+interface EventData {
+  eventId: number;
   title: string;
   location: string;
   date: string;
   description: string;
 }
 
-// Type the props of the function
 const Events: NextPage<{ params: EventPageParams }> = ({ params }) => {
-  const eventId = parseInt(params.eventId);
+  const eventId = params.eventId;
 
-
-  const [eventData, setEventData] = useState(params);
+  const [eventData, setEventData] = useState<EventData | null>(null);
 
   useEffect(() => {
     if (eventId) {
-      // Fetch event data
       fetch(`https://weddingbells-api.onrender.com/event/${eventId}`)
-      //fetch(`http://localhost:8080/events/${eventId}`)
         .then(response => response.json())
-        .then(data => {
-          setEventData(data);
-          console.log(data);
-        })
+        .then(data => setEventData(data))
         .catch(error => console.error('Error fetching event:', error));
     }
   }, [eventId]);
 
   return (
     <div>
-      <h1>Event ID: {eventData.title}</h1>
-      {/* Additional content */}
-      {/* <h1>Event Name: {eventData}</h1> */}
+      {eventData ? (
+        <div>
+          <h1>Event ID: {eventData.title}</h1>
+          {/* Additional content based on eventData */}
+        </div>
+      ) : (
+        <div>Loading event data...</div>
+      )}
     </div>
   );
 };
 
 export default Events;
-
-  
