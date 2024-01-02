382214307d9be4e1ee85f89f69f3d33b16aac67b
Connor Sheets <connorsheets32@gmail.com>
2024-01-02 08:06:48 -0700

    redirect to events/1 from homepage

--- Frontend/whatthesheets/app/page.tsx
+++ Frontend/whatthesheets/app/page.tsx
@@ -111,3 +111,14 @@
 //     </main>
 //   )
 // }
+import { permanentRedirect } from 'next/navigation';
+
+export default function Page() {
+  
+  permanentRedirect('/events/1');
+
+  
+  return <div>Redirecting...</div>;
+}
+
+
