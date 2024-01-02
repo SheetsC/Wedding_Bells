fdc8efa83d935440327d6dc40bab6c9143b08930
Connor Sheets <connorsheets32@gmail.com>
2024-01-02 10:42:36 -0700

    remove eventdata background

--- Frontend/whatthesheets/app/events/[eventId]/page.tsx
+++ Frontend/whatthesheets/app/events/[eventId]/page.tsx
@@ -52,7 +52,7 @@ const Events: NextPage<{ params: EventPageParams }> = ({ params }) => {
       <div className="relative z-10 pt-32 pb-8 min-h-screen">
         <div className="max-w-4xl mx-auto overflow-auto">
           {eventData ? (
-            <div className="text-center p-4 bg-white bg-opacity-80 rounded-lg shadow-lg">
+            <div className="text-center p-4 rounded-lg shadow-lg">
               <h1 className="text-xl font-bold">{eventData.title}</h1>
               {/* Additional content based on eventData */}
               <RsvpForm eventData={eventData} />
