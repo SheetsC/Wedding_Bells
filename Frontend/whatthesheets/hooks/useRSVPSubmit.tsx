9a0995d090e90c185befa0ea7574e9006881eb6c
Connor Sheets <connorsheets32@gmail.com>
2024-01-14 16:05:59 -0700

    new email ready

--- Frontend/whatthesheets/hooks/useRSVPSubmit.tsx
+++ Frontend/whatthesheets/hooks/useRSVPSubmit.tsx
@@ -51,7 +51,7 @@ export const useRSVPSubmit = () => {
           plus_one: values.plusOneName,
           event_date: "June 22nd, 2024"
           };
-          emailjs.send('service_kandmwedding', 'template_60ncaoc', emailData, '8zUT3hCrM_RlE28_E')
+          emailjs.send('service_6s68lyc', 'template_hwqsyd3', emailData, 'PL0L55K1X_S-gA9n8')
           .then((response) => {
               console.log('Email sent successfully!', response.status, response.text);
           }, (err) => {
